c6ac342dfd3732174ccc212295c0d2816df22cca
M. Scott Ford <scott@corgibytes.com>
2022-05-02 16:21:59 -0400

    Adds code used in 'Brevity vs Clarity' slides

---
+++ clean-names/01-brevity-vs-clarity/loops-clean.js
@@ -0,0 +1,11 @@
+const fruits = [
+  'apple',
+  'banana',
+  'cherry',
+  'date'
+];
+
+for (let index = 0; index < fruits.length; index++) {
+  const fruit = fruits[index];
+  console.log(fruit);
+}
---
+++ clean-names/01-brevity-vs-clarity/loops-messy.js
@@ -0,0 +1,11 @@
+const fruits = [
+  'apple',
+  'banana',
+  'cherry',
+  'date'
+];
+
+for (let i = 0; i < fruits.length; i++) {
+  const f = fruits[i];
+  console.log(f);
+}
