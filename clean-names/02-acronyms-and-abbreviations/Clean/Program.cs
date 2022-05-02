3ed093a60d9ba21eeba411817e84e8c1a1548c46
M. Scott Ford <scott@corgibytes.com>
2022-05-02 16:22:31 -0400

    Adds abbreviations and acronyms example

---
+++ clean-names/02-acronyms-and-abbreviations/Clean/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Threading.Tasks;
+
+namespace Clean
+{
+    class HttpApiClient
+    {
+        static void Main()
+        {
+            var repositories = RetrieveRepositories("oreillymedia").Result;
+
+            foreach (var repository in repositories)
+            {
+                Console.WriteLine(repository.Name);
+            }
+        }
+
+        private static async Task<List<Repository>> RetrieveRepositories(
+            string githubOrganization)
+        {
+            SetHeaders();
+
+            var task = httpApiClient.GetStreamAsync(
+                $"https://api.github.com/orgs/{githubOrganization}/repos"
+            );
+
+            var serializer = new DataContractJsonSerializer(typeof(List<Repository>));
+            var repositories = serializer.ReadObject(await task) as List<Repository>;
+            return repositories;
+        }
+
+        private static void SetHeaders()
+        {
+            httpApiClient.DefaultRequestHeaders.Accept.Clear();
+            httpApiClient.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue(
+                    "application/vnd.github.v3+json"
+                )
+            );
+            httpApiClient.DefaultRequestHeaders.Add(
+                "User-Agent",
+                "Clean HttpApiClient"
+            );
+        }
+
+        private static readonly HttpClient httpApiClient = new HttpClient();
+    }
+
+    [DataContract(Name = "repo")]
+    public class Repository
+    {
+        [DataMember(Name = "name")]
+        public string Name;
+    }
+}
---
+++ clean-names/02-acronyms-and-abbreviations/Messy/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Threading.Tasks;
+
+namespace Messy
+{
+    class HTTPAPIClient
+    {
+        static void Main()
+        {
+            var repos = RetRepos("oreillymedia").Result;
+
+            foreach (var repo in repos)
+            {
+                Console.WriteLine(repo.Name);
+            }
+        }
+
+        private static async Task<List<Repo>> RetRepos(
+            string org)
+        {
+            SetHeads();
+
+            var tsk = httpapiclient.GetStreamAsync(
+                $"https://api.github.com/orgs/{org}/repos"
+            );
+
+            var lizer = new DataContractJsonSerializer(typeof(List<Repo>));
+            var repos = lizer.ReadObject(await tsk) as List<Repo>;
+            return repos;
+        }
+
+        private static void SetHeads()
+        {
+            httpapiclient.DefaultRequestHeaders.Accept.Clear();
+            httpapiclient.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue(
+                    "application/vnd.github.v3+json"
+                )
+            );
+            httpapiclient.DefaultRequestHeaders.Add(
+                "User-Agent",
+                "Messy HTTPAPICli"
+            );
+        }
+
+        private static readonly HttpClient httpapiclient = new HttpClient();
+    }
+
+    [DataContract(Name = "repo")]
+    public class Repo
+    {
+        [DataMember(Name = "name")]
+        public string Name;
+    }
+}
