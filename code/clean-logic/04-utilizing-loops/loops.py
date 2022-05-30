e2d1d634bfb3d733c67ae6c4662feab7262d3c57
M. Scott Ford <scott@corgibytes.com>
2022-05-30 14:52:16 -0400

    Adds loops sample

---
+++ code/clean-logic/04-utilizing-loops/loops.py
@@ -0,0 +1,26 @@
+class Person:
+    def __init__(self):
+      self.name = None
+      self.email = None
+      self.age = None
+
+    def save:
+      pass
+
+person = Person()
+person.name = "Samantha"
+person.email = "sam@example.com"
+person.age = 45
+person.save
+
+person = Person()
+person.name = "Juan"
+person.email = "juan@example.com"
+person.age = 22
+person.save
+
+person = Person()
+person.name = "Alina"
+person.email = "alina@example.com"
+person.age = 12
+person.save
---
+++ code/clean-logic/04-utilizing-loops/loops_cleaner.py
@@ -0,0 +1,22 @@
+class Person:
+    def __init__(self):
+      self.name = None
+      self.email = None
+      self.age = None
+
+    def save:
+      pass
+
+people = [
+    { 'name': "Samantha", 'email': "sam@example.com", 'age': 45 },
+    { 'name': "Juan", 'email': "juan@example.com", 'age': 22 },
+    { 'name': "Alina", 'email': "alina@example.com", 'age': 12 }
+]
+
+for person_attributes in people:
+    person = Person()
+    person.name = person_attributes['name']
+    person.email = person_attributes['email']
+    person.age = person_attributes['age']
+    person.save
+
